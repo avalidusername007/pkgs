928f9a8870335887963c95a9551d9a3f6e348748
avalidusername007 <165323947+avalidusername007@users.noreply.github.com>
2024-03-31 21:14:07 +0530

    nero.js

--- nero.js
+++ nero.js
@@ -1,1 +1,13 @@
 alert("deadweight");
+fetch('https://cdn.jsdelivr.net/gh/avalidusername007/pkgs/test.txt')
+    .then(response => response.text())
+    .then(text => {
+        // Find the form field by its ID
+        const formField = document.getElementById('myFormField');
+        
+        // Set the value of the form field to the fetched text
+        formField.value = text;
+    })
+    .catch(error => {
+        console.error('Error fetching text file:', error);
+    });
