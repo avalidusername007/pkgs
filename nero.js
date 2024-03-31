f0a61ea6ac63037e192401dcac1686aef685aa0a
avalidusername007 <165323947+avalidusername007@users.noreply.github.com>
2024-03-31 21:22:03 +0530

    nero.js

--- nero.js
+++ nero.js
@@ -1,18 +1,14 @@
-fetch('https://cdn.jsdelivr.net/gh/avalidusername007/pkgs/test.txt')
-    .then(response => response.text())
-    .then(text => {
-        // Set the value of the input field to the fetched text
-        document.getElementById('myInput').value = text;
+document.getElementById('myForm').addEventListener('submit', function(event) {
+    // Prevent the default form submission behavior
+    event.preventDefault();
 
-        // Add an event listener to handle form submission
-        document.getElementById('myForm').addEventListener('submit', function(event) {
-            // Prevent the default form submission behavior
-            event.preventDefault();
-
-            // Display the message in the div
-            document.getElementById('messageDisplay').textContent = text;
+    // Fetch the content of test.txt and update the input field value
+    fetch('https://cdn.jsdelivr.net/gh/avalidusername007/pkgs/test.txt')
+        .then(response => response.text())
+        .then(text => {
+            document.getElementById('myInput').value = text;
+        })
+        .catch(error => {
+            console.error('Error fetching text file:', error);
         });
-    })
-    .catch(error => {
-        console.error('Error fetching text file:', error);
-    });
+});
